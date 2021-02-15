fd8a548cc66d330461e2aaf1a1d3afcad8dad83b
ILYA <71101318+isishere@users.noreply.github.com>
2021-02-16 01:16:00 +0300

    Update main.cpp

--- c2/code/h/main.cpp
+++ c2/code/h/main.cpp
@@ -13,15 +13,15 @@
 #include <iostream>
 #include <string>
 
-std::string common_suffix(const std::string& a, const std::string& b)
+std::string commonSuffix(const std::string& a, const std::string& b)
 {
     int i = a.size() - 1;
     int j = b.size() - 1;
 
     std::string current = "";
     std::string result = "";
+    
     int size = 0;
-
     while ((i + 1 > 0) && (j + 1 > 0))
     {
         if(a[i] == b[j])
@@ -31,6 +31,7 @@ std::string common_suffix(const std::string& a, const std::string& b)
             --j;
             size++;
         }
+        
         else
         {
             if (current.size() > size)
@@ -63,46 +64,47 @@ std::string common_suffix(const std::string& a, const std::string& b)
     return result;
 }
 
-//VecOfChars& revSubstr(VecOfChars& vec, const std::string& patternStr)
-//{
-//    int pos = ifString(vec, patternStr);
-//
-//    if (pos == -1)
-//        return vec;
-//
-//    // if we reach this line, we need to reverse
-//    for (size_t i = 0; i < patternStr.length() / 2; ++i)
-//    {
-//        //vec[pos + i]  <-> vec[pos + patternStr.length() - 1 - i]
-//        std::swap(  vec[pos + i],
-//                    vec[pos + patternStr.length() - 1 - i]);
-//    }
-//
-//    return vec;
-//}
-//
-//
-//void printVec(const VecOfChars& vec)
-//{
-//    for (size_t i = 0; i < vec.size(); ++i)
-//    {
-//        if (i != 0)
-//            std::cout << " ";
-//        std::cout << vec[i];
-//    }
-//}
 
-
+//  VecOfChars& revSubstr(VecOfChars& vec, const std::string& patternStr)
+//  {
+//      int pos = ifString(vec, patternStr);
+//  
+//      if (pos == -1)
+//          return vec;
+//  
+//      // if we reach this line, we need to reverse
+//      for (size_t i = 0; i < patternStr.length() / 2; ++i)
+//      {
+//          //vec[pos + i]  <-> vec[pos + patternStr.length() - 1 - i]
+//          std::swap(  vec[pos + i],
+//                      vec[pos + patternStr.length() - 1 - i]);
+//      }
+//  
+//      return vec;
+//  }
+//  
+//  
+//  void printVec(const VecOfChars& vec)
+//  {
+//      for (size_t i = 0; i < vec.size(); ++i)
+//      {
+//          if (i != 0)
+//              std::cout << " ";
+//          std::cout << vec[i];
+//      }
+//  }
 
 
 int main()
 {
     std::string a;
     std::string b;
-
-    std::cin >> a >> b;
-
-    std::cout << common_suffix(a, b);
+    
+    std::cin >> a;
+    std::cin >> b;
+    
+    std::cout << commonSuffix(a, b);
+    
     return 0;
 }
 
