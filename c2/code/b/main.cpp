aee72b536c6782b5688d40393de271a586e9140e
ILYA <71101318+isishere@users.noreply.github.com>
2021-02-16 00:07:35 +0300

    Update main.cpp

--- c2/code/b/main.cpp
+++ c2/code/b/main.cpp
@@ -9,10 +9,36 @@
 ///
 ////////////////////////////////////////////////////////////////////////////////
 
+#include <iostream>
+#include <string>
+#include <sstream>
+
 
 int main()
 {
-    // TODO: provide your solution here.
+    std::string str;
+    std::getline(std::cin, str);
+    std::stringstream ss(str);
+
+    std::string curr;
+    std::string string = "";
+    while(ss.good() && !ss.eof())
+    {
+        ss >> curr;
+        string += curr;
+    }
+
+
+    for (int i = 0; i < string.size(); i++)
+    {
+        if (string[i] != string[string.size() - i - 1])
+        {
+            std::cout << "no";
+            exit(0);
+        }
+    }
+
+    std::cout << "yes";
 
     return 0;
 }
